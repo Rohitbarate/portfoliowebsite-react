23f72a3f8d682fc08ff8087f7696ef0738a99123
Rohit Rajendra Barate <rohitbarate100@gmail.com>
2022-05-28 21:39:29 +0530

    first

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,26 +5,26 @@ import { Link } from 'react-scroll'
 import { motion } from "framer-motion"
 // import photo from '../img/icon-1.png'
 const Home = () => {
-  const transition = { duration: 2,type: 'spring'}
+  const transition = { duration: 2, type: 'spring' }
   return (
     <div className='home section' id='home'>
       <div className="left">
         <div className="center">
-          <motion.div className="temp1" initial={{ left:'-100px' }}
-  whileInView={{ left: '0px' }}
-  transition={transition}>
-    <p></p> 
+          <motion.div className="temp1" initial={{ left: '-100px' }}
+            whileInView={{ left: '0px' }}
+            transition={transition}>
+            <p></p>
           </motion.div>
           <motion.div className="temp2"
-           initial={{ right:'-100px' }}
-  whileInView={{ right: '20px' }}
-  transition={transition}>
-    <div className="temp2-front">
-    <p>😊</p>
-    </div>
-    <div className="temp2-back">
-    <p>🥰</p>
-    </div>
+            initial={{ right: '-100px' }}
+            whileInView={{ right: '20px' }}
+            transition={transition}>
+            <div className="temp2-front">
+              <p>😊</p>
+            </div>
+            <div className="temp2-back">
+              <p >🥰</p>
+            </div>
           </motion.div>
         </div>
       </div>
